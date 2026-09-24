d3b42d9c60d8c752930e8cc0ac0e96b5efd55a41
ChaiR88 <chair1988@gmail.com>
2026-09-24 23:20:57 +0800

    Project commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+### Description
+
+Project Etch-A-Sketch from The Odin Project.
+
+Using HTML, CSS and Javascript.
+
---
+++ script.js
@@ -0,0 +1,115 @@
+
+let squaresSize = 16;
+
+const buttonForSquares = document.querySelector(".change-number-squares");
+const buttonToClearColors = document.querySelector(".clear-colors");
+const modalBox = document.getElementById("custom-modal");
+const inputSquares = document.getElementById("input-squares");
+const continueBtn = document.getElementById("continue-button");
+const cancelBtn = document.getElementById("cancel-button");
+
+
+function createSquares(squaresSize) {
+    
+    const squaresContainer = document.querySelector(".squares-container");
+    squaresContainer.replaceChildren(); //clears the existing squares
+
+    const totalSquares = squaresSize * squaresSize;
+    const sizePercentage = 100 / squaresSize;
+    
+    for (let i = 0; i < totalSquares; i++) {
+        const eachSquare = document.createElement("div");
+        eachSquare.className = "square";
+        eachSquare.style.flex = `0 0 ${sizePercentage}%`;
+        eachSquare.style.aspectRatio = "1 / 1";
+        squaresContainer.appendChild(eachSquare);
+
+        eachSquare.addEventListener('mouseenter', () => {
+            eachSquare.style.backgroundColor = createColorsHex();
+            
+        });
+
+
+        
+
+
+        eachSquare.addEventListener('click', () => {
+            eachSquare.style.backgroundColor = '';
+        });
+
+
+        buttonToClearColors.addEventListener('click', () => {
+            eachSquare.style.backgroundColor = '';
+        })
+
+
+    }
+
+}
+
+
+function createColorsSoftRGB() {
+    const red = Math.floor(Math.random() * 76) + 180;
+    const blue = Math.floor(Math.random() * 76) + 180;
+    const green = Math.floor(Math.random() * 76) + 180;
+
+    return `rgb(${red},${blue},${green})`;
+
+}
+
+
+function createColorsHex() {
+    const base = Math.floor(Math.random() * 16777216);
+    return `#${base.toString(16)}`;
+
+}
+
+
+
+inputSquares.value = squaresSize;
+inputSquares.select(); //just highlight the entered text so user can easily change text
+createSquares(squaresSize);
+
+
+buttonForSquares.addEventListener('click', () => {
+    inputSquares.value = squaresSize; //takes care of the case where when I enter an invalid number and I click Cancel and open the modal again, the invalid number shows up
+    inputSquares.select(); //just highlight the entered text so user can easily change text
+    modalBox.showModal();
+    
+})
+
+
+continueBtn.addEventListener('click', () => {
+    const input = Number(inputSquares.value);
+    inputSquares.select(); //just highlight the entered text so user can easily change text
+
+    if (isNaN(input) || input < 2 || input > 100) {
+        alert("Please enter a valid number between 2 and 100");
+
+    }
+    else {
+        squaresSize = inputSquares.value;
+        createSquares(squaresSize);
+        modalBox.close();
+
+    }
+
+})
+
+//when pressing the Enter key
+inputSquares.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        continueBtn.click();
+    }
+
+})
+
+
+cancelBtn.addEventListener('click', () => {
+    inputSquares.select(); //just highlight the entered text so user can easily change text
+    modalBox.close();
+
+})
+
+
